bc0190cb68a33b916980ddc404ceeadde3a5c81d
Sarath Kumar <sarathkumark365>
2023-02-06 20:55:06 +0530

    switch

--- src/rightBoard.jsx
+++ src/rightBoard.jsx
@@ -6,7 +6,7 @@ function RightBoard(props) {
     const storedDataArr = Object.values(storedData).map((el,i) => {
       return (<p key={i}>{el}</p>)
     })
-    // console.log(storedDataArr);
+    // console.log(st oredDataArr);
 
 
     return (
