8d2a5fa20b433af6934b3b6460ddcdd92f6959f4
Sarath Kumar <sarathkumark365>
2023-02-07 17:20:04 +0530

    into the css part

--- src/rightBoard.jsx
+++ src/rightBoard.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 
 function RightBoard(props) {
 
-  const dataToDisplayArr = props.dataToDisplay.map((el,i) => <p key={i}>{el}</p>)
+  const dataToDisplayArr = props.dataToDisplay.map((el,i) => {
+    return (
+      <>
+        <div className='databox'>
+          <p key={i}>{el}</p>
+        </div>
+      </>
+    )
+  })
 
     return (
     <div className="right-board">
         {dataToDisplayArr}
+        {/* <div className="databox">
+          <p>data ...</p>
+        </div> */}
+
     </div>
   )
 }
