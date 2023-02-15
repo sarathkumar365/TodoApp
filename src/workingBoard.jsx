b31b436191c4451addb79fbdd4c73bd4b10c0b48
sarathkumar365 <sarathkumark.s365@gmail.com>
2023-02-15 16:07:52 +0530

    bttns animations done

--- src/workingBoard.jsx
+++ src/workingBoard.jsx
@@ -131,10 +131,10 @@ function WorkingBorard() {
           <div className="header-section--left">
             <div className="header-name">TODO</div>
             <div className="bttns">
-              <a onClick={() => addTask()} className="add" href="#">
+              <a onClick={() => addTask()} className="add button" href="#">
                 Add
               </a>
-              <a onClick={() => delTask()} className="del" href="#">
+              <a onClick={() => delTask()} className="del button" href="#">
                 Del
               </a>
             </div>
